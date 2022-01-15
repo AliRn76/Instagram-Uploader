d31653ad71eede4314acfff562cc2fc08c08e7a0
AliRn76 <alirn76@yahoo.com>
2022-01-15 20:15:47 +0330

    try to fix import

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from discord.ext import commands
+from ..app import final_image
 
 valid_insta_url = "https://www.instagram.com/"
 
@@ -10,10 +11,12 @@ class InstaBot(commands.Cog):
 
     @commands.command(name='url')
     async def url(self, ctx, url):
+        print(f'{url = }')
         if valid_insta_url == url[:26]:
             image_path = final_image(url)
-            await ctx.send(file=discord.File("/root/dev/Instagram-Uploader/" + image_path))
-            os.remove("/root/dev/Instagram-Uploader/" + image_path)
+            print(f'{image_path = }')
+            await ctx.send(file=discord.File("../" + image_path))
+            os.remove("../" + image_path)
         else:
             await ctx.reply("Bad URL")
 
